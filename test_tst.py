4efa9e8af779493fe0255c710081e33aa0288d8b
= <28640600@qq.com>
2018-09-06 14:30:36 +0800

    learn to write test class

--- test_tst.py
+++ test_tst.py
@@ -1,4 +1,4 @@
-#coding = utf-8
+# coding = utf-8
 
 import unittest
 from tst import Tst
@@ -13,10 +13,12 @@ class TestTst(unittest.TestCase):
         print('this is teardown')
 
     def test_get_pathname(self):
+        print('testing get pathname')
         self.assertEqual(self.t.get_pathname(), 'sddd')
 
-   # def test_get_nothing(self):
-   #     self.assertEqual(self.t.get_nothing(), None)
+    def test_get_nothing(self):
+        print('testing get nothing')
+        self.assertEqual(self.t.get_nothing(), 'nothing')
 
 
 if __name__ == '__main__':
